66d08e31b90847cca3c94b5745f23e1bc1fa7a24
MridulGupta007 <mridulgupta2003official@gmail.com>
2024-03-01 17:46:07 +0530

    ui change

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ import pfp from "./assets/bear.png";
 function App() {
   const [length, setLength] = useState(0);
   const [joke, setJoke] = useState("");
+  const [jokes, setJokes] = useState([])
   const [address, setAddress] = useState(
     "0xc707B5466Af7E156d97B3dF59FdbdC4C8F4744cC"
   );
+
+  const addJoke = () => {
+    // add user address here
+    setJokes(prev => [...prev, {user: address, joke: joke} ])
+  }
   return (
     <div className="flex">
       <Header />
@@ -20,6 +26,7 @@ function App() {
               className="border relative px-5 py-3 resize-none focus:outline-none outline-none"
               cols={50}
               rows={5}
+              maxLength={100}
               placeholder="Let's have a laugh together?!"
               onChange={(event) => {
                 setJoke(event.target.value);
@@ -30,7 +37,10 @@ function App() {
               <span className="relative self-end bottom-2 right-24 text-[10px] text-slate-500">
                 {length}/100
               </span>
-              <button className="bg-black self-end px-3 py-2 text-white rounded-md relative right-[90px] bottom-2">
+              <button className="bg-black self-end px-3 py-2 text-white rounded-md relative right-[90px] bottom-2"
+              onClick={() => {
+                addJoke()
+              }}>
                 Post
               </button>
             </div>
@@ -38,79 +48,44 @@ function App() {
         </div>
         {/* Default Post Box ends */}
 
-        <div className="flex py-10 justify-center gap-x-5">
-          <img
-            src={pfp}
-            alt="profile-pic"
-            className="w-16 self-start rounded-full"
-          />
-          <div className="flex flex-col justify-center gap-y-1">
-            <span>
-              {address.slice(0, 5)}....
-              {address.slice(address.length - 5, address.length)}
-            </span>
-            <textarea
-              className="border relative px-5 py-3 resize-none focus:outline-none outline-none"
-              cols={50}
-              rows={5}
-              placeholder="Let's have a laugh together?!"
-            ></textarea>
+        {/* rendering of jokes starts here */}
 
-            <button className="bg-black self-end px-3 py-2 text-white rounded-md relative right-2 bottom-12">
-              Subscribe
-            </button>
-          </div>
+        <div>
+          {jokes.length > 0 ? (jokes.map((elem, index) => {
+            return(
+              <div className="flex py-10 justify-center gap-x-5" key={index}>
+              <img
+                src={pfp}
+                alt="profile-pic"
+                className="w-16 self-start rounded-full"
+              />
+              <div className="flex flex-col justify-center gap-y-1">
+                <span>
+                  {elem.user.slice(0, 5)}....
+                  {elem.user.slice(address.length - 5, address.length)}
+                </span>
+                <textarea
+                  className="border relative px-5 py-3 resize-none focus:outline-none outline-none"
+                  cols={50}
+                  rows={5}
+                  placeholder={elem.joke}
+                ></textarea>
+    
+                <button className="bg-black self-end px-3 py-2 text-white rounded-md relative right-2 bottom-12">
+                  Subscribe
+                </button>
+              </div>
+            </div>
+            )
+          })) : (
+            <div className="text-center text-[20px] font-light">
+              Sorry no jokes at the moment
+            </div>
+          )}
         </div>
+ 
 
 
-        <div className="flex py-10 justify-center gap-x-5">
-          <img
-            src={pfp}
-            alt="profile-pic"
-            className="w-16 self-start rounded-full"
-          />
-          <div className="flex flex-col justify-center gap-y-1">
-            <span>
-              {address.slice(0, 5)}....
-              {address.slice(address.length - 5, address.length)}
-            </span>
-            <textarea
-              className="border relative px-5 py-3 resize-none focus:outline-none outline-none"
-              cols={50}
-              rows={5}
-              placeholder="Let's have a laugh together?!"
-            ></textarea>
-
-            <button className="bg-black self-end px-3 py-2 text-white rounded-md relative right-2 bottom-12">
-              Subscribe
-            </button>
-          </div>
-        </div>
-
-
-        <div className="flex py-10 justify-center gap-x-5">
-          <img
-            src={pfp}
-            alt="profile-pic"
-            className="w-16 self-start rounded-full"
-          />
-          <div className="flex flex-col justify-center gap-y-1">
-            <span>
-              {address.slice(0, 5)}....
-              {address.slice(address.length - 5, address.length)}
-            </span>
-            <textarea
-              className="border relative px-5 py-3 resize-none focus:outline-none outline-none"
-              cols={50}
-              rows={5}
-              placeholder="Let's have a laugh together?!"
-            ></textarea>
-
-            <button className="bg-black self-end px-3 py-2 text-white rounded-md relative right-2 bottom-12">
-              Subscribe
-            </button>
-          </div>
-        </div>
       </div>
     </div>
   );
