793252e36122ba54d1588a88e795f4e16fb0fdfd
Ankit Choudhary <sign.ankit14@gmail.com>
2024-03-04 10:52:30 +0530

    Updated smart contract

--- Contract/Contract.sol
+++ Contract/Contract.sol
@@ -2,11 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-contract Twitter {
+contract Connect {
     struct User{
         address[] subscribed;
-        string name;
-        string bio;
         string[] tweets;
         uint claimableAmount;
         bool oldUser;
@@ -27,51 +25,34 @@ contract Twitter {
     }
 
     function claimAmount() public payable{
-        payable(msg.sender).transfer(users[msg.sender].claimableAmount);
         users[msg.sender].claimableAmount=0;
 
     }
 
     function subscribe(address _user) public payable{
-        require(msg.value == 0.001 ether, "Incorrect payment amount");
         users[_user].subscribers.push(msg.sender);
         users[msg.sender].subscribed.push(_user);
-        users[_user].claimableAmount = users[_user].claimableAmount + 800000000000000;
-    }
-
-    function setName(string memory _name) public{
-        users[msg.sender].name = _name;
-    }
-
-    function setBio(string memory _bio) public{
-        users[msg.sender].bio = _bio;
+        users[_user].claimableAmount = users[_user].claimableAmount + 800000000000000000;
     }
 
     function getAllUsers() public view returns(address[] memory){
         return allUsers;
     }
 
-    function getSubscribedAccounts() public view returns(address[] memory){
-        return users[msg.sender].subscribed;
+    function getSubscribedAccounts(address accountAddress) public view returns(address[] memory){
+        return users[accountAddress].subscribed;
     }
 
-    function getSubscribers() public view returns(address[] memory){
-        return users[msg.sender].subscribers;
+    function getSubscribers(address accountAddress) public view returns(address[] memory){
+        return users[accountAddress].subscribers;
     }
 
-    function getName() public view returns(string memory){
-        return users[msg.sender].name;
+
+    function getTweets(address accountAddress) public view returns(string[] memory){
+        return users[accountAddress].tweets;
     }
 
-    function getBio() public view returns(string memory){
-        return users[msg.sender].bio;
-    }
-
-    function getTweets() public view returns(string[] memory){
-        return users[msg.sender].tweets;
-    }
-
-    function getClaimAmount() public view returns(uint256){
-        return users[msg.sender].claimableAmount;
+    function getClaimAmount(address accountAddress) public view returns(uint256){
+        return users[accountAddress].claimableAmount;
     }
 }
